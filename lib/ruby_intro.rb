e941ef11aa899dfdf03be21a26ee5770d8d148a5
omarkhwaja <omar.khwaja@gmail.com>
2015-11-07 17:41:04 +0000

    Completed Part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,28 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each { |elem|
+    sum += elem
+  }
+  sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  case arr.length
+  when 0 then return 0
+  when 1 then return arr[0]
+  else        return arr.sort[-1] + arr.sort[-2]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return n == 0 ? true : false if arr.empty?
+  return false if arr.length == 1
+  arr.combination(2) do |a,b|
+    return true if (a+b) == n
+  end
+  return false
 end
 
 # Part 2
